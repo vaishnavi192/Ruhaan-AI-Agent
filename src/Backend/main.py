1f76032f3512bece20ab14708642bf2c3a521cc4
vaishnavi maheshwari <117008958+vaishnavi192@users.noreply.github.com>
2025-07-02 23:31:37 +0530

    backend

--- src/Backend/main.py
+++ src/Backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # CORS middleware to allow frontend to talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://ruhaan-ai-agent.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
